9d10d24b16d25cef29751d565453dc4d81778fd6
Daniel <wi19281lehre.dhbw-stuttgart.de>
2021-04-12 15:45:31 +0200

    Changes by benschaper

--- src/main/java/carmanagement/cockpit/dealer/DealerController.java
+++ src/main/java/carmanagement/cockpit/dealer/DealerController.java
@@ -1,11 +1,30 @@
 package carmanagement.cockpit.dealer;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import carmanagement.cockpit.car.Car;
+import carmanagement.cockpit.car.CarService;
+import carmanagement.cockpit.car.dto.Position;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/dealer")
 @CrossOrigin("*")
 public class DealerController {
+
+    @Autowired
+    private DealerService dealerService;
+
+    // getAllCars
+    @GetMapping("")
+    public List<Dealer> getAllDealers(){
+        return dealerService.findAll();
+    }
+
+    @PostMapping("")
+    public Long saveDealer(@RequestBody Dealer dealer) {
+        return dealerService.saveDealer(dealer).getId();
+    }
 }
--- src/main/java/carmanagement/cockpit/dealer/DealerService.java
+++ src/main/java/carmanagement/cockpit/dealer/DealerService.java
@@ -1,7 +1,23 @@
 package carmanagement.cockpit.dealer;
 
+import carmanagement.cockpit.car.Car;
+import carmanagement.cockpit.car.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DealerService {
+
+    @Autowired
+    private DealerRepository repository;
+
+    public Dealer saveDealer(Dealer dealer) {
+        return repository.save(dealer);
+    }
+
+    public List<Dealer> findAll() {
+        return repository.findAll();
+    }
 }
