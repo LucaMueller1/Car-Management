93785134ef50dc895fe66647f4773092709dacb6
Daniel <wi19281lehre.dhbw-stuttgart.de>
2021-04-16 14:22:36 +0200

    NewBackendTest

---
+++ WeatherBackend/src/test/java/de/carmanagement/weatherbackend/WeatherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.carmanagement.weatherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
