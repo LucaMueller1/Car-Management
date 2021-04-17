4976bc45585a8f735926eb2e480601046dd0988c
Daniel <wi19281lehre.dhbw-stuttgart.de>
2021-04-17 16:18:55 +0200

    Finished Weather Backend

--- WeatherBackend/src/main/java/de/carmanagement/weatherbackend/WeatherRisk.java
+++ WeatherBackend/src/main/java/de/carmanagement/weatherbackend/WeatherRisk.java
@@ -1,26 +1,47 @@
 package de.carmanagement.weatherbackend;
 
+import java.util.Date;
+
 public class WeatherRisk {
 
     Double latitude;
     Double longitude;
-    Integer score;
-    Integer wind;
-    Double temp;
+    Double score;
+    Double wind;
+    Double minTemp;
+    Double maxTemp;
     Double rainProbability;
+    Date date;
 
-    public WeatherRisk(Double latitude, Double longitude, Integer score, Integer wind, Double temp, Double rainProbability) {
+    public WeatherRisk(Double latitude, Double longitude, Double score, Double wind, Double minTemp, Double rainProbability) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.score = score;
         this.wind = wind;
-        this.temp = temp;
+        this.minTemp = minTemp;
         this.rainProbability = rainProbability;
     }
 
     public WeatherRisk(Double latitude, Double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
+        this.score = 0.0;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(Double maxTemp) {
+        this.maxTemp = maxTemp;
     }
 
     public Double getLatitude() {
@@ -39,28 +60,34 @@ public class WeatherRisk {
         this.longitude = longitude;
     }
 
-    public Integer getScore() {
+    public Double getScore() {
         return score;
     }
+    public void increaseScore(Double increment){
+        if (this.score >= 1.0) return;
+        Double newscore = this.score + increment;
+        if (newscore > 1.0) this.score = 1.0;
+        else this.score = newscore;
+    }
 
-    public void setScore(Integer score) {
+    public void setScore(Double score) {
         this.score = score;
     }
 
-    public Integer getWind() {
+    public Double getWind() {
         return wind;
     }
 
-    public void setWind(Integer wind) {
+    public void setWind(Double wind) {
         this.wind = wind;
     }
 
-    public Double getTemp() {
-        return temp;
+    public Double getMinTemp() {
+        return minTemp;
     }
 
-    public void setTemp(Double temp) {
-        this.temp = temp;
+    public void setMinTemp(Double minTemp) {
+        this.minTemp = minTemp;
     }
 
     public Double getRainProbability() {
--- WeatherBackend/src/main/java/de/carmanagement/weatherbackend/WeatherRiskController.java
+++ WeatherBackend/src/main/java/de/carmanagement/weatherbackend/WeatherRiskController.java
@@ -3,23 +3,25 @@ package de.carmanagement.weatherbackend;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 @RestController
 public class WeatherRiskController {
@@ -29,6 +31,11 @@ public class WeatherRiskController {
         WeatherRisk returned = retrieveFromApi(weatherriskreceived);
 
         return returned;
+
+    }
+    @RequestMapping(value = "/calculateWeatherRiskFordays", method = RequestMethod.POST)
+    public ArrayList<WeatherRisk> calculateWeatherRisk(@RequestBody WeatherRiskDTO weatherriskreceived , @RequestParam Integer numberOfDays) {
+        return retriveForDaysFromApi(weatherriskreceived,numberOfDays);
 
     }
 
@@ -47,13 +54,102 @@ public class WeatherRiskController {
             HttpResponse httpresponse = httpclient.execute(get);
             System.out.println("Status of the request for weather infos: " + httpresponse.getStatusLine());
             String responseBody = EntityUtils.toString(httpresponse.getEntity(), StandardCharsets.UTF_8);
-            System.out.println("WeatherInfoResponse: " + responseBody);
+            //System.out.println("WeatherInfoResponse: " + responseBody);
             JSONObject response = new JSONObject(responseBody);
-            System.out.println(response);
+            JSONArray consolidatedWeather = new JSONArray(response.getJSONArray("consolidated_weather"));
+
+            LocalDate dt = new LocalDate();
+
+            for (Object obj : consolidatedWeather){
+                JSONObject currentobj = (JSONObject) obj;
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                Date tocompare = formatter.parse(currentobj.getString("applicable_date"));
+                if (Days.daysBetween(dt, new LocalDate(tocompare)).getDays() == 0 ){
+                    System.out.println(currentobj + System.lineSeparator() + System.lineSeparator());
+                    double min_temp= currentobj.getDouble("min_temp");
+
+                    if (min_temp < 10.0){
+                        Double temp_score= (1/min_temp) * 0.5;
+                        returned.increaseScore(temp_score);
+                        System.out.println("Temperature score:" + temp_score);
+                    }
+                    if (currentobj.getString("weather_state_abbr").equals("hr")) returned.increaseScore(0.2);
+                    if (currentobj.getString("weather_state_abbr").equals("hs") || currentobj.getString("weather_state_abbr").equals("sl")) returned.increaseScore(0.4);
+                    System.out.println("Final score for date: " +  formatter.format(tocompare)  + " "+ returned.getScore());
+                    returned.setMinTemp(currentobj.getDouble("min_temp"));
+                    returned.setMaxTemp(currentobj.getDouble("max_temp"));
+                    returned.setWind(currentobj.getDouble("wind_speed"));
+                    returned.setDate(tocompare);
+                }
+
+
+            }
         } catch (URISyntaxException uriSyntaxException) {
             uriSyntaxException.printStackTrace();
         } catch (IOException ioException) {
             ioException.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        return returned;
+    }
+
+    public ArrayList<WeatherRisk> retriveForDaysFromApi(WeatherRiskDTO weatherriskreceived, int dayScopeFromNow){
+        ArrayList<WeatherRisk> returned =  new ArrayList<>();
+        Integer nearest = retrieveNearestCity(weatherriskreceived.getLatitude(),weatherriskreceived.getLongitude());
+        URIBuilder builder = null;
+        try {
+            builder = new URIBuilder("https://www.metaweather.com/api/location/" + nearest + "/");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet get = new HttpGet(builder.build());
+            HttpResponse httpresponse = httpclient.execute(get);
+            System.out.println("Status of the request for weather infos: " + httpresponse.getStatusLine());
+            String responseBody = EntityUtils.toString(httpresponse.getEntity(), StandardCharsets.UTF_8);
+            //System.out.println("WeatherInfoResponse: " + responseBody);
+            JSONObject response = new JSONObject(responseBody);
+            JSONArray consolidatedWeather = new JSONArray(response.getJSONArray("consolidated_weather"));
+
+            LocalDate dt = new LocalDate();
+
+            for (Object obj : consolidatedWeather){
+                JSONObject currentobj = (JSONObject) obj;
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                Date tocompare = formatter.parse(currentobj.getString("applicable_date"));
+                if (Days.daysBetween(dt, new LocalDate(tocompare)).getDays() <= dayScopeFromNow ){
+                    WeatherRisk toadd = new WeatherRisk(weatherriskreceived.getLatitude(), weatherriskreceived.getLongitude());
+                    System.out.println(currentobj + System.lineSeparator() + System.lineSeparator());
+                    double min_temp= currentobj.getDouble("min_temp");
+
+                    if (min_temp < 10.0){
+                        Double temp_score= (1/min_temp) * 0.5;
+                        toadd.increaseScore(temp_score);
+                        System.out.println("Temperature score:" + temp_score);
+                    }
+                    if (currentobj.getString("weather_state_abbr").equals("hr")) toadd.increaseScore(0.2);
+                    if (currentobj.getString("weather_state_abbr").equals("hs") || currentobj.getString("weather_state_abbr").equals("sl")) toadd.increaseScore(0.4);
+                    System.out.println("Final score for date: " +  formatter.format(tocompare)  + " "+ toadd.getScore());
+                    toadd.setMinTemp(currentobj.getDouble("min_temp"));
+                    toadd.setMaxTemp(currentobj.getDouble("max_temp"));
+                    toadd.setWind(currentobj.getDouble("wind_speed"));
+                    toadd.setDate(tocompare);
+
+                    returned.add(toadd);
+                }
+
+
+            }
+        } catch (URISyntaxException uriSyntaxException) {
+            uriSyntaxException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
 
@@ -78,9 +174,9 @@ public class WeatherRiskController {
 
 
             //Printing the status line
-            System.out.println(httpresponse.getStatusLine());
+           // System.out.println(httpresponse.getStatusLine());
             String responseBody = EntityUtils.toString(httpresponse.getEntity(), StandardCharsets.UTF_8);
-            System.out.println("Response body: " + responseBody);
+           // System.out.println("Response body: " + responseBody);
             JSONArray response = new JSONArray(responseBody);
             JSONObject nearestcity= (JSONObject) response.get(0);
             woeidofnearestcity = (Integer) nearestcity.get("woeid");
